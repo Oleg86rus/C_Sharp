ff6aee709c8c03986cab54050214f2d2beac75e0
oleg086rus <o.pototskiy@yandex.ru>
2023-01-05 03:50:06 +0500

    sem 6

---
+++ hw/hw_6/task_1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//
+// 0, 7, 8, -2, -2 -> 2
+//
+// 1, -7, 567, 89, 223-> 3
+
+void PositiveCounter(int[] array) {
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) result++;
+    }
+    Console.Write($"[{string.Join(",", array)}] -> ");
+    Console.WriteLine(result);
+}
+
+int[] CreateArray(int[] array) {
+    int [] result = {};
+    for (int i = 0; i + 1 > 0 ; i++) {
+        Console.Write("ВВедите число, или нажмите Enter для завершения: ");
+        string str = Console.ReadLine();
+        int var = 0;
+        if (str == "") break;
+        else {
+                Array.Resize(ref array, i+1);
+                var = Int32.Parse(str);
+                array[i] = var;
+        }
+    }
+    return array;
+}
+
+int[] array = {};
+array = CreateArray(array);
+PositiveCounter(array);
---
+++ hw/hw_6/task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями
+// y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+//
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void Intersection(double k1, double b1, double k2, double b2)
+{
+double x = (b1-b2)/(k2-k1);
+double y = (k2 * x) + b2;
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {y})");
+}
+
+double Data(string text)
+{
+    Console.Write(text);
+    double var = double.Parse(Console.ReadLine());
+    return var;
+}
+double b1 = Data("b1 ");
+double k1 = Data("k1 ");
+double b2 = Data("b2 ");
+double k2 = Data("k2 ");
+Intersection(k1, b1, k2, b2);
