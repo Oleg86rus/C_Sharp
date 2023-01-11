06a1c28e29e09d081c26aa511013c233d31ab4a6
oleg086rus <o.pototskiy@yandex.ru>
2023-01-11 15:26:40 +0500

    hw 8 task 4-5

--- hw/hw_8/task_4/Program.cs
+++ hw/hw_8/task_4/Program.cs
@@ -7,3 +7,40 @@
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
+void PrintArray(int[, ,] array) {
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            for (int c = 0; c < array.GetLength(2); c++) {
+                Console.Write($"{array[i,j,c]}({i},{j},{c}) ");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
+
+int[,,] UniqueData() {
+    int minValue = 10;
+    int maxValue = 99;
+    Random rand = new Random();
+    List<int> temp = new List<int>();
+    int[,,] array = new int [2,2,2];
+
+    for (int i = minValue; i < maxValue; i++) {
+        temp.Add(i);
+    }
+
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            for (int b = 0; b < array.GetLength(2); b++) {
+                array[i,j,b] = temp[rand.Next(0,temp.Count)];
+                temp.Remove(array[i,j,b]);
+            }
+        }
+    }
+    Console.WriteLine("Рандомный массив из неповторяющихся двузначных чисел:");
+
+    return array;
+}
+
+int[,,] array = UniqueData();
+PrintArray(array);
--- hw/hw_8/task_5/Program.cs
+++ hw/hw_8/task_5/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+void PrintArray(int[,] array) {
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            if (array[i, j]< 10){Console.Write($"0{array[i, j]} ");}
+            else {Console.Write($"{array[i, j]} ");}
+        }
+        Console.WriteLine("");
+    }
+}
+
+int[,] Spiral(int[,] array) {
+    int row = array.GetLength(0);
+    int col = array.GetLength(1);
+    int count = 1;
+    for (int i = 0, j = 0, k = 0; row != 0; i++,j++) {
+        for (k = 0; k < col - 1; k++) {array[i, j++] = count++;}
+        for (k = 0; k < row - 1; k++) {array[i++, j] = count++;}
+        for (k = 0; k < col - 1; k++) {array[i, j--] = count++;}
+        for (k = 0; k < row - 1; k++) {array[i--, j] = count++;}
+        row = row < 2 ? 0 : row - 2;
+        col = col < 2 ? 0 : col - 2;
+    }
+
+    return array;
+}
+
+int[,] array = new int [4,4];
+array = Spiral(array);
+PrintArray(array);
